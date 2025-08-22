a5a1f62be1b2c43ab218ae15ee5e273c65a4a41f
Michanoku <michael.werker@gmail.com>
2025-08-22 04:55:58 +0000

    Add project functions

--- src/projects.js
+++ src/projects.js
@@ -1,14 +1,29 @@
 const projects = (function () {
+  // Create array to house all projects
   const projectsArray = new Array();
+
   class Project {
     constructor(name) {
       this.name = name;
       this.id = crypto.randomUUID();
+      // All todo objects will go in here
       this.todo = new Array();
     }
   }
 
   function createProject(name) {
     projectsArray.append(new Project(name));
-  }
+  };
+
+  function getAllProjects() {
+    return projectsArray;
+  };
+
+  function getProject(id) {
+    return projectsArray.find((project) => project.id === id);
+  };
+
+
+
+  return { createProject, getAllProjects, getProject }
 })();
