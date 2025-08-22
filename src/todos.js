4b0de3652804f40189b5d0c9c9c605b1673531df
Michanoku <michael.werker@gmail.com>
2025-08-22 07:09:04 +0000

    Add todo class and creator

--- src/todos.js
+++ src/todos.js
@@ -1,10 +1,29 @@
-  import getProject from './projects.js'
+import { compareAsc, format } from "date-fns";
 
-  const todos = (function () {
-    class Todo {
-      constructor(/*TODO (hah!)*/) {
-      }
+const todos = (function () {
+  class Todo {
+    constructor(
+      title='Untitled',
+      description=null,
+      dueDate=null,
+      priority='Normal',
+      notes=new Array(), 
+      checklist=new Array(),
+    ) {
+      this.title = title;
+      this.description = description;
+      this.dueDate = dueDate === null ? null : format(new Date(dateString), "yyyy-MM-dd");
+      this.priority = priority;
+      this.notes = notes;
+      this.checklist = checklist;
+      this.checked = false;
     }
+  }
 
-  }());
+  function createTodo(title, description, dueDate, priority, notes, checklist) {
+    return new Todo(title, description, dueDate, priority, notes, checklist);
+  }
+
+  return { createTodo }
+}());
   
